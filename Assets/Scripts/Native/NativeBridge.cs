7de7faea2074c922ef0de7d9be33ddc10e44636f
JeongSuhong <ghjkdfe@gmail.com>
2020-01-24 14:30:04 +0900

    Upload Call Toas Message

---
+++ Assets/Scripts/Native/NativeBridge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NativeBridge : MonoBehaviour
+{
+    private static NativeBridge instance;
+    public static NativeBridge Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new NativeBridge();
+
+            return instance;
+        }
+    }
+
+    private AndroidJavaObject unityActivity;
+
+    private AndroidJavaObject push;
+
+
+    private void Awake()
+    {
+        instance = this;
+
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        unityActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+        AndroidJavaClass pushJC = new AndroidJavaClass("com.example.roarman.pushmanager.PushController");
+        push = pushJC.CallStatic<AndroidJavaObject>("Instance");
+    }
+
+    public void SetToast(string message)
+    {
+        unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            push.Call("ShowToast", unityActivity, message);
+        }));
+    }
+}
---
+++ Assets/Scripts/SampleManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleManager : MonoBehaviour
+{
+public void OnClickToast()
+    {
+        NativeBridge.Instance.SetToast("토스트 메세지!");
+    }
+}
